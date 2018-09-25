ecd655b63116350181bc5534d9740de293390232
rmanny <ryan.manny@wsu.edu>
2018-09-25 12:19:56 -0700

    Refactor letter_demo into one file, pivot project

--- letter_demo.py
+++ letter_demo.py
@@ -1,5 +1,5 @@
 import pygame
-import util
+import random
 
 RESOLUTION = 800, 800
 
@@ -9,14 +9,23 @@ SIZE_STEP = 3
 MOVEMENT_SPEED = 7
 
 
-def main():
+def get_random_char(size, color):
+    char = chr(random.randint(33, 127))
+    return pygame.font.Font(pygame.font.get_default_font(), size).render(char, False, color)
+
+
+def get_random_color():
+    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
+
+def play():
     pygame.init()
     screen = pygame.display.set_mode(RESOLUTION)
     buffer = pygame.Surface(RESOLUTION)
 
     size = STARTING_SIZE
 
-    background = util.get_random_color()
+    background = get_random_color()
     screen.fill(background)
 
     while True:
@@ -31,8 +40,8 @@ def main():
             if size > SIZE_LIMIT:
                 size = STARTING_SIZE
 
-            color = util.get_random_color()
-            char = util.get_random_char(size, color)
+            color = get_random_color()
+            char = get_random_char(size, color)
 
             w, h = char.get_size()
             pos = pygame.mouse.get_pos()
@@ -52,7 +61,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    play()
 
 
 # rainbow mystical T
